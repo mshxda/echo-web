6abc0c7d1c7515e31d53c55887e68b8e092e6819
mshxda <mariadruzinina140@gmail.com>
2025-03-29 00:43:24 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,34 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let draggableElements = document.querySelectorAll(".send-1");
-  
-    draggableElements.forEach(function (element) {
-      let isDragging = false;  
-      let offsetX, offsetY;    
-      element.addEventListener("mousedown", function (event) {
-        isDragging = true;    
-  
-        offsetX = event.clientX - element.getBoundingClientRect().left;
-       
-        offsetY = event.clientY - element.getBoundingClientRect().top;
-       
-        function onMouseMove(event) {
-          if (isDragging) {
-            let x = event.clientX - offsetX;
-            let y = event.clientY - offsetY;
-  
-            element.style.left = x + "px";
-            element.style.top = y + "px";
-          }
-        }
-  
-        function onMouseUp() {
-          isDragging = false; 
-          document.removeEventListener("mousemove", onMouseMove);
-          document.removeEventListener("mouseup", onMouseUp);
-        }
-  
-        document.addEventListener("mousemove", onMouseMove);
-        document.addEventListener("mouseup", onMouseUp);
-      });
+document.addEventListener("DOMContentLoaded", () => {
+
+let isDragging = false;
+  let activeElement = null;
+  let startX = 0, startY = 0;
+
+  document.querySelectorAll(".dragg").forEach((img) => {
+
+    img.ondragstart = () => false;
+
+    img.addEventListener("mousedown", (e) => {
+      e.preventDefault();
+      isDragging = true;
+      activeElement = img;
+
+      startX = e.clientX - activeElement.offsetLeft;
+      startY = e.clientY - activeElement.offsetTop;
+
+      activeElement.style.position = "absolute";
+      activeElement.style.zIndex = "1000";
     });
-  }); 
+
+    document.addEventListener("mousemove", (e) => {
+      if (isDragging && activeElement) {
+        activeElement.style.left = e.clientX - startX + "px";
+        activeElement.style.top = e.clientY - startY + "px";
+      }
+    });
+
+    document.addEventListener("mouseup", () => {
+      isDragging = false;
+      activeElement = null;
+    });
+  });
+
+
+
+  
+});
+
+
